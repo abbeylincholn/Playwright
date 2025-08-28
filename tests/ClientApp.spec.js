8529a5c2bbf65572c4d9a28e04c43bb57894e324
abbey <abbeyezekiel100@gmail.com>
2025-08-28 14:33:14 +0100

    Select dropdown, Radio button, checkbox, espect assertions and validation

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -16,3 +16,27 @@ const { test, expect } = require('@playwright/test');
        console.log(titles); 
      
     })
+
+test.only('UI Controls', async ({ page }) => {
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+  await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+  
+  const username = page.locator('#username');
+  const password = page.locator('#password');
+  const signInBtn = page.locator('#signInBtn');
+  const documentLink = page.locator("[href*='documents-request']");
+  const userDropdown = page.locator("select.form-control");
+  await userDropdown.selectOption('consult'); 
+  await page.locator(".radiotextsty").last().click();
+  await page.locator("#okayBtn").click();
+  await expect(page.locator(".radiotextsty").last()).toBeChecked();
+  console.log( await page.locator(".radiotextsty").last().isChecked());
+
+  await page.locator("#terms").click();
+  await expect(page.locator("#terms")).toBeChecked();
+  await page.locator("#terms").uncheck();
+  expect(await page.locator('#terms').isChecked()).toBeFalsy();
+  console.log( await page.locator("#terms").isChecked());
+  await expect (documentLink).toHaveAttribute("class","blinkingText");
+
+});
