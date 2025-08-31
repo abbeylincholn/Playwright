bd1722e91a59c3cfbf4bda432e11c2f8e5453964
abbey <abbeyezekiel100@gmail.com>
2025-08-31 14:53:39 +0100

    Filter Element by getby Role, Text, Label

--- tests/SpecialLocator.spec.js
+++ tests/SpecialLocator.spec.js
@@ -5,7 +5,16 @@ test('Special Locators', async ({page})=>{
     await page.getByLabel("Check me out if you Love IceCreams!").click();
     await page.getByLabel("Employed").check();
     await page.getByLabel("Gender").selectOption("Female");
-    // await page.getByPlaceholder("Type to search").fill("ind");
+    await page.getByPlaceholder("Password").fill("ind");
+    await page.getByRole("button", {name: "Submit"}).click();
+    await page.getByText("Success! The Form has been submitted successfully!").isVisible();
+    await page.getByRole("link", {name: "Shop"}).click();
+
+    await page.locator("app-card").filter({hasText: "iphone X"}).getByRole("button").click();
+    await page.locator("app-card").filter({hasText: "Blackberry"}).getByRole("button", {name: "Add"}).click();
+
+
+
     // const dropdown = page.locator(".ui-menu-item-wrapper");
     // await dropdown.nth(1).click();
     // await expect(page.locator("#autocomplete")).toHaveValue("India");
