f5672d288df20387b7c5d8caadfb499ded6da242
abbey <abbeyezekiel100@gmail.com>
2025-09-07 17:02:22 +0100

    Update Excel file

--- ExcelJSUtils/ExcelDemo.js
+++ ExcelJSUtils/ExcelDemo.js
@@ -14,15 +14,26 @@ const ExcelJs = require('exceljs');
 
 // or 
 
-async function readExcel() {
+// read file
+async function readExcel() {    
     const workbook = new ExcelJs.Workbook();
     await workbook.xlsx.readFile("C:/Users/abbey/Downloads/download.xlsx");
     const worksheet = workbook.getWorksheet('Sheet1');
     worksheet.eachRow((row, rowNumber) => {
         row.eachCell((cell, colNumber) => {
-            console.log(cell.value);
+            if (cell.value === "Apple") {
+                console.log(rowNumber);
+                console.log(colNumber);
+
+            }
         })
     })
+
+
+    // update file
+    const cell = worksheet.getCell(3,2);
+    cell.value = "Pineapple";
+    await workbook.xlsx.writeFile("C:/Users/abbey/Downloads/download.xlsx");
 }
 
 readExcel();
